05c4a78522252a3489d4543a58ca586a493dd90e
Egor <ekurito@gmail.com>
2017-10-20 23:07:58 +0300

    redy to deploy

--- config.py
+++ config.py
@@ -3,3 +3,8 @@ BOT_TOKEN = '462824235:AAEmB2CobyniIHloVd39COgakeUIYkzJFJ8'
 
 #URL
 url = 'https://api.bitfinex.com/v1/pubticker/'
+
+# URL Heroku
+URL = 'HEROKU APP URL'
+# Port
+PORT = "int(os.environ.get('PORT', '5000'))"
